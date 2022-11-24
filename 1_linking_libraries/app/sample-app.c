8af89f363e8de90d76d472c98eb5b1af1e5c6060
Vijay K Panchal <vijaykantibhai@yahoo.in>
2022-11-24 04:23:39 +0000

    Updated : App Printstring and Makefile

--- 1_linking_libraries/app/sample-app.c
+++ 1_linking_libraries/app/sample-app.c
@@ -6,6 +6,6 @@ int main (int argc, char *argv[])
 {
     int N = 10;	
     int result = find_squre(N);
-    printf("Square of %d is %d !/n", N, result);
+    printf("Square of %d is %d !\n", N, result);
 	return 0;
 }
